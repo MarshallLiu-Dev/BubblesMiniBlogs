5d47da870d5d1b76287e58576146478c839bc8bf
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-05-16 11:52:58 -0300

    first commit

--- src/hooks/useauthentication.js
+++ src/hooks/useauthentication.js
@@ -82,6 +82,41 @@ export const useAuthentication = () =>{
         }
 
 
+// login sing in 
+
+    const login = async (data) => {
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+        } catch (error) {
+            console.log(error.message);
+            console.log(typeof error.message);
+            console.log(error.message.includes("user-not"));
+
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado.";
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta.";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+            }
+
+            console.log(systemErrorMessage);
+
+            setError(systemErrorMessage);
+        }
+
+        console.log(error);
+
+        setLoading(false);
+    };
+
 
 
 
@@ -96,6 +131,6 @@ export const useAuthentication = () =>{
         error,
         loading,
         logout,
-        // login,
+        login,
     };
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,55 @@
 import React from 'react'
 //css
 import Styles from './Home.module.css' 
+import { useState } from "react";
+
+// hooks
+import { useFetchDocuments } from "./../../hooks/useauthentication";
+import { useNavigate, Link } from "react-router-dom";
+import { useFetchDocuments }
+
+// react
+
+
+// components
+// import PostDetail from "../../components/PostDetail";
+
 
 const Home = () => {
+
+  // const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();}
+
   return (
     <div className={Styles.home}>
-      <h3>Detalhes do Carro</h3>
-      <ul>
-        <li>Marca: </li>
-        <li>Km: </li>
-        <li>Cor: </li>
-      </ul>
+
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={Styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post)=><h3>{post.title}</h3>)}
+        {posts && posts.length === 0 &&  (
+          <div className={Styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post </Link>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+
+  };
+
 
 export default Home
